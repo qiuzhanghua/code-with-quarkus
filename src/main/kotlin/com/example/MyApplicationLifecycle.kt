c0efa69354ec4330b56350a3408db4206eeebe5d
Daniel <qiuzhanghua@icloud.com>
2019-11-28 18:11:19 +0800

    Add Server init in ApplicationLifecycle

--- src/main/kotlin/com/example/MyApplicationLifecycle.kt
+++ src/main/kotlin/com/example/MyApplicationLifecycle.kt
@@ -1,30 +1,33 @@
 package com.example
 
-import io.quarkus.arc.ArcContainer
-import io.vertx.core.Vertx
+import io.quarkus.runtime.StartupEvent
 import java.util.*
 import javax.enterprise.context.ApplicationScoped
-import javax.enterprise.context.Destroyed
 import javax.enterprise.context.Initialized
 import javax.enterprise.event.Observes
 
 
 @ApplicationScoped
 class MyApplicationLifecycle {
-    private val users: MutableMap<String, String> = HashMap<String, String>()
+//    private val users: MutableMap<String, String> = HashMap<String, String>()
 
-    fun init(@Observes @Initialized(ApplicationScoped::class) init: Any?) {
-//        var  c = init as ArcContainer
-
-        println("*".repeat(40))
-        println(init!!::class)
-        println(init)
-//        println(c.beanManager().getBeans("io.vertx.core.Vertx"))
-        println("*".repeat(40))
-        users["cdi"] = "CDI"
+    fun onStart(@Observes ev: StartupEvent?, tcpServer: Server) {
+        // For singleton injection is enough
+        // For @ApplicationScoped beans a client proxy is injected and so we need to invoke some method to force bean instantiation
+        tcpServer.init()
     }
-
-    fun destroy(@Observes @Destroyed(ApplicationScoped::class) init: Any?) {
-        users.clear()
-    }
+//    fun init(@Observes @Initialized(ApplicationScoped::class) init: Any?) {
+////        var  c = init as ArcContainer
+//
+//        println("*".repeat(40))
+//        println(init!!::class)
+//        println(init)
+////        println(c.beanManager().getBeans("io.vertx.core.Vertx"))
+//        println("*".repeat(40))
+//        users["cdi"] = "CDI"
+//    }
+//
+//    fun destroy(@Observes @Destroyed(ApplicationScoped::class) init: Any?) {
+//        users.clear()
+//    }
 }
--- src/main/kotlin/com/example/Server.kt
+++ src/main/kotlin/com/example/Server.kt
@@ -2,14 +2,20 @@ package com.example
 
 import io.vertx.core.Vertx
 import javax.annotation.PostConstruct
+import javax.ejb.Startup
 import javax.enterprise.context.ApplicationScoped
+import javax.enterprise.context.Destroyed
+import javax.enterprise.context.Initialized
+import javax.enterprise.event.Observes
 import javax.enterprise.inject.Default
 import javax.inject.Inject
 import javax.inject.Singleton
 
 //@Singleton
+//@Startup
 @ApplicationScoped
 open class Server {
+
     @Inject
     @field: Default
     lateinit var vertx: Vertx
@@ -18,11 +24,23 @@ open class Server {
         println("in static init")
     }
 
-    @PostConstruct
     open fun init() {
         println("init")
+        vertx.createHttpServer()
+                .requestHandler { req ->
+                    req.response().end("Hello!")
+                }
+                .listen(9090)
         var server = vertx.createNetServer()
-        server.listen(1234, "localhost") { res ->
+        server.connectHandler { socket ->
+            socket.handler { buffer ->
+                println("I received some bytes: ${buffer.length()}")
+            }
+            socket.closeHandler { _ ->
+                println("The socket has been closed")
+            }
+        }
+        server.listen(9999, "0.0.0.0") { res ->
             if (res.succeeded()) {
                 println("Server is now listening!")
             } else {
@@ -30,4 +48,14 @@ open class Server {
             }
         }
     }
+
+
+//    open fun init2(@Observes @Initialized(ApplicationScoped::class) init: Any) {
+//        println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+//        println(init.toString())
+//    }
+//
+//        open fun destroy(@Observes @Destroyed(ApplicationScoped::class) init: Any) {
+//        println("#######################")
+//    }
 }
